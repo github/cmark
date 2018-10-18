9160ee35a4fd51b4c9eb15eded3b6042eb9c7181
Phil Turnbull <philipturnbull@github.com>
2018-10-18 12:48:25 -0400

    Load GFM extensions in fuzz harness

--- test/cmark-fuzz.c
+++ test/cmark-fuzz.c
@@ -1,6 +1,21 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include "cmark-gfm.h"
+#include "cmark-gfm-core-extensions.h"
+
+const char *extension_names[] = {
+  "autolink",
+  "strikethrough",
+  "table",
+  "tagfilter",
+  NULL,
+};
+
+int LLVMFuzzerInitialize(int *argc, char ***argv) {
+  cmark_gfm_core_extensions_ensure_registered();
+  return 0;
+}
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   struct __attribute__((packed)) {
@@ -16,7 +31,6 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     fuzz_config.options &= (
       CMARK_OPT_SOURCEPOS |
       CMARK_OPT_HARDBREAKS |
-      CMARK_OPT_SAFE |
       CMARK_OPT_NOBREAKS |
       CMARK_OPT_NORMALIZE |
       CMARK_OPT_VALIDATE_UTF8 |
@@ -26,13 +40,28 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       CMARK_OPT_LIBERAL_HTML_TAG |
       CMARK_OPT_FOOTNOTES |
       CMARK_OPT_STRIKETHROUGH_DOUBLE_TILDE |
-      CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES
+      CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES |
+      CMARK_OPT_FULL_INFO_STRING |
+      CMARK_OPT_UNSAFE
     );
 
     /* Remainder of input is the markdown */
     const char *markdown = (const char *)(data + sizeof(fuzz_config));
     const size_t markdown_size = size - sizeof(fuzz_config);
-    cmark_node *doc = cmark_parse_document(markdown, markdown_size, fuzz_config.options);
+    cmark_parser *parser = cmark_parser_new(fuzz_config.options);
+
+    for (const char **it = extension_names; *it; ++it) {
+      const char *extension_name = *it;
+      cmark_syntax_extension *syntax_extension = cmark_find_syntax_extension(extension_name);
+      if (!syntax_extension) {
+        fprintf(stderr, "%s is not a valid syntax extension\n", extension_name);
+        abort();
+      }
+      cmark_parser_attach_syntax_extension(parser, syntax_extension);
+    }
+
+    cmark_parser_feed(parser, markdown, markdown_size);
+    cmark_node *doc = cmark_parser_finish(parser);
 
     free(cmark_render_commonmark(doc, fuzz_config.options, fuzz_config.width));
     free(cmark_render_html(doc, fuzz_config.options, NULL));
@@ -41,6 +70,7 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     free(cmark_render_xml(doc, fuzz_config.options));
 
     cmark_node_free(doc);
+    cmark_parser_free(parser);
   }
   return 0;
 }
